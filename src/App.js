cb47f48281c5f70a17b0fa3d04db6962d774593c
Cinkasihaloho1 <cinkasihaloho@gmail.com>
2022-11-11 19:38:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Main from "./pages/main";
 
 function App() {
+
+  const [chat, setChat] = useState([]);
+  const [leftChat, setLeftChat] = useState({});
+  const [rightChat, setRightChat] = useState({});
+
+
+  function sendToLeft(){
+    setChat([...chat, leftChat]);
+    document.getElementById('input-left').value = ''
+  }
+
+  function sendToRight(){
+    setChat([...chat, rightChat]);
+    document.getElementById('input-right').value = ''
+  }
+
+  const title = {
+    fontSize: '24pt',
+    color: 'white',
+    fontWeight: 'bold',
+    paddingLeft: '1em'
+  }
+
+  const subTitle = {
+    fontSize: '16pt',
+    color: 'white',
+    paddingLeft: '1.5em'
+  }
+
+  const leftInput = {
+    display: 'flex',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+  const rightInput = {
+    display: 'flex',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+
+  const container = {
+    display: 'flex'
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p style={title}>
+        AU MAKER
+      </p>
+      <Main chat={chat}/>
+      <div style={container }>
+        <div style={leftInput}>
+          <div style={leftInput}>
+            <input  id="input-left" onChange={(e) => setLeftChat({message: e.target.value, type: 'left'})}/>
+            <button onClick={sendToLeft}>Send </button>
+          </div>
+        </div>
+
+        <div style={rightInput}>
+          <div style={rightInput}>
+            <input id="input-right" onChange={(e) => setRightChat({message: e.target.value, type: 'right'})}/>
+            <button  onClick={sendToRight}>Send </button>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
